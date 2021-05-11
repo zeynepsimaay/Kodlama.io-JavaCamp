aba4d8f7e26a27af251cb1bc8c822d15f6814e60
Zeynep Simay KILINÇ <76596710+zeynepsimaay@users.noreply.github.com>
2021-05-11 11:14:07 +0300

    Update and rename hw to Course.java

---
+++ Lecture2/Course.java
@@ -0,0 +1,15 @@
+public class Course {
+
+  public Course(String courseName, String instructorName, double price, String detail) {
+
+    this.courseName = courseName;
+    this.instructorName = instructorName;
+    this.price = price;
+    this.detail = detail;
+  }
+
+  String courseName;
+  String instructorName;
+  double price;
+  String detail;
+}

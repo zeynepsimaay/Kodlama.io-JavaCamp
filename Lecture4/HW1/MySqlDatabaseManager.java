9337657fb676c51ac3af762c836a59f3e9bda9c0
Zeynep Simay KILINÇ <76596710+zeynepsimaay@users.noreply.github.com>
2021-05-19 12:44:33 +0300

    abstractDemo

---
+++ Lecture4/HW1/MySqlDatabaseManager.java
@@ -0,0 +1,9 @@
+package abstractDemo;
+
+public class MySqlDatabaseManager  extends BaseDatabaseManager {
+	public void getData() {
+		System.out.println("Veri getirildi : MySql");
+		
+	}
+
+}
---
+++ Lecture4/HW1/SqlServeDatabaseManager.java
@@ -0,0 +1,9 @@
+package abstractDemo;
+
+public class SqlServeDatabaseManager extends BaseDatabaseManager {
+	public void getData() {
+		System.out.println("Veri getirildi : Sql Server");
+		
+	}
+
+}

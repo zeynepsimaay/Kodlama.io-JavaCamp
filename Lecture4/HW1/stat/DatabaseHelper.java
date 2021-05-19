f5345a9d04cf62f3b29198907c2cffb14ac8f415
Zeynep Simay KILINÇ <76596710+zeynepsimaay@users.noreply.github.com>
2021-05-19 12:50:42 +0300

    Add files via upload

---
+++ Lecture4/HW1/stat/DatabaseHelper.java
@@ -0,0 +1,23 @@
+package staticDemo;
+
+public class DatabaseHelper {
+	//inner class
+		public static class Crud{//Create Read Update Delete
+			
+			public static void Delete() {
+				
+			}
+			public static void Update() {
+				
+			}
+		
+		}
+		public static class Connection {
+			public static void createConnection() {
+				
+			}	
+		}
+			
+			
+		
+}

9257a25aac3dc67f57043082f8eb5c8e74894b62
Zeynep Simay KILINÇ <76596710+zeynepsimaay@users.noreply.github.com>
2021-05-25 20:39:33 +0300

    Add files via upload

---
+++ Lecture4/HW2/Main.java
@@ -0,0 +1,22 @@
+package interfaceAbstractDemo;
+
+import java.time.LocalDate;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter() );
+		
+		
+		customerManager.Save(new Customer( "Zeynep Simay", "Kýlýnç", LocalDate.of(1998,11,10), "23185923520"));
+		
+	}
+
+}
